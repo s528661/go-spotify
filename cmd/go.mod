db1cc6f16cdbeb0eae609cdbc4359f5e69e428be
s528661 <e535061u@gmail.com>
2024-04-09 23:16:30 +0900

    20240409

--- cmd/go.mod
+++ cmd/go.mod
@@ -1,5 +1,11 @@
 module example.com
 
-go 1.14
+go 1.19
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/zmb3/spotify/v2 v2.4.1
+	golang.org/x/oauth2 v0.19.0
+)
+
+require github.com/stretchr/testify v1.8.1 // indirect
--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
+
+	"golang.org/x/oauth2/clientcredentials"
+
 	"github.com/joho/godotenv"
+	"github.com/zmb3/spotify/v2"
 )
 
 func main() {
@@ -13,9 +20,28 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	id := os.Getenv("SPOTIFY_ID")
-	secret := os.Getenv("SPOTIFY_SECRET")
 
-	fmt.Println(id)
-	fmt.Println(secret)
+	ctx := context.Background()
+	config := &clientcredentials.Config{
+		ClientID:     os.Getenv("SPOTIFY_ID"),
+		ClientSecret: os.Getenv("SPOTIFY_SECRET"),
+		TokenURL:     spotifyauth.TokenURL,
+	}
+	token, err := config.Token(ctx)
+	if err != nil {
+		log.Fatalf("couldn't get token: %v", err)
+	}
+
+	httpClient := spotifyauth.New().Client(ctx, token)
+	client := spotify.New(httpClient)
+
+	// The BeatlesのSpotify IDを指定
+	result, err := client.GetRelatedArtists(ctx, "3WrFJ7ztbogyGnTHbHJFl2")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bytes, _ := json.MarshalIndent(result[0:2], "", "  ")
+	fmt.Println(string(bytes))
 }
+
